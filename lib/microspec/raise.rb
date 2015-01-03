0c6a95591487c6f294c6ce50e56d26ab31a7b6ec
Erol Fornoles <erol.fornoles@gmail.com>
2015-01-04 01:44:30 +0800

    Move exception generation to helper module

--- lib/microspec/raise.rb
+++ lib/microspec/raise.rb
@@ -2,6 +2,20 @@ require 'microspec/flunked'
 
 module Microspec
   module Raise
+    module Helper
+      def self.exception(expected: nil, actual: nil, message: nil)
+        if actual.nil?
+          Flunked.new 'missing exception', expected: expected, actual: actual
+        elsif not actual.is_a? expected.class
+          Flunked.new 'unexpected exception', expected: expected, actual: actual
+        elsif message.is_a? String and not actual.message == message
+          Flunked.new 'unexpected exception message', expected: expected, actual: actual
+        elsif message.is_a? Regexp and not actual.message =~ message
+          Flunked.new 'unexpected exception message', expected: expected, actual: actual
+        end
+      end
+    end
+
     module Method
       def raises(expected = Exception, message = nil)
         expected = expected.new message if expected.is_a? Class
@@ -10,15 +24,7 @@ module Microspec
 
       rescue Exception => actual
       ensure
-        exception = if actual.nil?
-                      Flunked.new 'missing exception', expected: expected, actual: actual
-                    elsif not actual.is_a? expected.class
-                      Flunked.new 'unexpected exception', expected: expected, actual: actual
-                    elsif message.is_a? String and not actual.message == message
-                      Flunked.new 'unexpected exception message', expected: expected, actual: actual
-                    elsif message.is_a? Regexp and not actual.message =~ message
-                      Flunked.new 'unexpected exception message', expected: expected, actual: actual
-                    end
+        exception = Helper.exception expected: expected, actual: actual, message: message
 
         raise exception if exception
       end
