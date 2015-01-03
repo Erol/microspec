a63a41d17b75ad4cb0b81d232c71b411c05ca7c8
Erol Fornoles <erol.fornoles@gmail.com>
2015-01-03 15:29:16 +0800

    Allow initialization with metadata

--- lib/microspec/exceptions.rb
+++ lib/microspec/exceptions.rb
@@ -1,4 +1,15 @@
 module Microspec
   class Flunked < StandardError
+    def metadata
+      @_metadata ||= {}
+    end
+
+    def initialize(message = nil, **metadata)
+      super message
+
+      metadata.each do |key, value|
+        self.metadata[key] = value
+      end
+    end
   end
 end
