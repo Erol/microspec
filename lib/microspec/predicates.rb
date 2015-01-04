70e0f682a0e887ec66d6b6150555325ad9e138cc
Erol Fornoles <erol.fornoles@gmail.com>
2015-01-04 13:49:28 +0800

    Add Microspec predicate helpers

---
+++ lib/microspec/predicates.rb
@@ -0,0 +1,13 @@
+module Microspec
+  module Predicates
+    @_predicates = {}
+
+    def self.[](method)
+      @_predicates[method]
+    end
+
+    def self.[]=(method, proc)
+      @_predicates[method] = proc
+    end
+  end
+end
