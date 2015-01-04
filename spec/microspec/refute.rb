5db767e443f1245bdc470a78bffd0c13af35a45a
Erol Fornoles <erol.fornoles@gmail.com>
2015-01-04 18:23:03 +0800

    Use predicate helpers and flunk if true

--- spec/microspec/refute.rb
+++ spec/microspec/refute.rb
@@ -5,3 +5,15 @@ spec do
     refutes([1, 2, 3]).include? 3
   end
 end
+
+spec do
+  raises Microspec::Flunked, 'failed assert' do
+    refutes(true).truthy?
+  end
+end
+
+spec do
+  raises Microspec::Flunked, 'failed assert' do
+    refutes(false).falsey?
+  end
+end
