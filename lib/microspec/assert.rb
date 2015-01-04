9e1dffe5f45b17d0c4895f83f42be0d500ab3b94
Erol Fornoles <erol.fornoles@gmail.com>
2015-01-04 14:02:39 +0800

    Use predicate helpers and flunk if false

--- lib/microspec/assert.rb
+++ lib/microspec/assert.rb
@@ -1,3 +1,5 @@
+require 'microspec/predicates'
+
 module Microspec
   module Assert
     module Method
@@ -14,6 +16,18 @@ module Microspec
       def method_missing(method, *expected, &block)
         unless @_actual.send method, *expected
           raise Flunked.new 'failed assert', actual: @_actual, method: method, expected: expected
+        end
+
+      rescue Exception => exception
+      ensure
+        if exception.is_a? Flunked
+          raise exception
+        elsif Predicates[method]
+          unless Predicates[method].call @_actual, *expected, &block
+            raise Flunked.new 'failed assert', actual: @_actual, method: method, expected: expected
+          end
+        elsif exception
+          raise exception
         end
       end
     end
--- spec/microspec/assert.rb
+++ spec/microspec/assert.rb
@@ -5,3 +5,15 @@ spec do
     asserts([1, 2, 3]).include? 4
   end
 end
+
+spec do
+  raises Microspec::Flunked, 'failed assert' do
+    asserts(true).falsey?
+  end
+end
+
+spec do
+  raises Microspec::Flunked, 'failed assert' do
+    asserts(false).truthy?
+  end
+end
