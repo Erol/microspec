d93a38aee87884a8ed8dfe15b16ab33997065491
Erol Fornoles <erol.fornoles@gmail.com>
2015-01-04 13:52:14 +0800

    Define assert method and object

--- lib/microspec/assert.rb
+++ lib/microspec/assert.rb
@@ -1,4 +1,15 @@
 module Microspec
   module Assert
+    module Method
+      def asserts(object, &block)
+        Object.new object, &block
+      end
+    end
+
+    class Object
+      def initialize(actual)
+        @_actual = actual
+      end
+    end
   end
 end
