cedce698cb671fcf7b7196e817e38cc5c05ad803
Erol Fornoles <erol.fornoles@gmail.com>
2015-01-05 07:51:27 +0800

    Merge assert and refute specs

---
+++ spec/microspec/expectation.rb
@@ -0,0 +1,83 @@
+require 'microspec'
+
+spec do
+  raises Microspec::Flunked, 'failed assert' do
+    asserts([1, 2, 3]).include? 4
+  end
+end
+
+spec do
+  raises Microspec::Flunked, 'failed assert' do
+    asserts(true).falsey?
+  end
+end
+
+spec do
+  raises Microspec::Flunked, 'failed assert' do
+    asserts(false).truthy?
+  end
+end
+
+spec do
+  raises Microspec::Flunked, 'failed assert' do
+    asserts(100) == 1000
+  end
+end
+
+spec do
+  asserts([1, 2, 3]).include? 3
+end
+
+spec do
+  asserts(true).truthy?
+end
+
+spec do
+  asserts(false).falsey?
+end
+
+spec do
+  asserts(100) == 100
+end
+
+
+
+spec do
+  raises Microspec::Flunked, 'failed assert' do
+    refutes([1, 2, 3]).include? 3
+  end
+end
+
+spec do
+  raises Microspec::Flunked, 'failed assert' do
+    refutes(true).truthy?
+  end
+end
+
+spec do
+  raises Microspec::Flunked, 'failed assert' do
+    refutes(false).falsey?
+  end
+end
+
+spec do
+  raises Microspec::Flunked, 'failed assert' do
+    refutes(100) == 100
+  end
+end
+
+spec do
+  refutes([1, 2, 3]).include? 4
+end
+
+spec do
+  refutes(true).falsey?
+end
+
+spec do
+  refutes(false).truthy?
+end
+
+spec do
+  refutes(100) == 1000
+end
