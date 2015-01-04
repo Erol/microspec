91bd6c4f18c2056ca21cc89127383e912e31d42d
Erol Fornoles <erol.fornoles@gmail.com>
2015-01-04 18:19:13 +0800

    Define refute method and object

--- lib/microspec/refute.rb
+++ lib/microspec/refute.rb
@@ -1,4 +1,15 @@
 module Microspec
   module Refute
+    module Method
+      def refutes(object, &block)
+        Object.new object, &block
+      end
+    end
+
+    class Object
+      def initialize(actual)
+        @_actual = actual
+      end
+    end
   end
 end
