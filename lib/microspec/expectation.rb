0ac73117a989d723b8cf9db2710ba7c1e82f40e7
Erol Fornoles <erol.fornoles@gmail.com>
2015-01-06 08:50:38 +0800

    Simplify expectation exception handling

--- lib/microspec/expectation.rb
+++ lib/microspec/expectation.rb
@@ -30,9 +30,7 @@ module Microspec
 
       rescue Exception => exception
       ensure
-        if exception.is_a? Flunked
-          raise exception
-        elsif Predicates[method]
+        if exception.instance_of? NoMethodError and Predicates[method]
           unless !!@_boolean == !!Predicates[method].call(@_actual, *expected, &block)
             raise Flunked.new "failed #{@_type}", actual: @_actual, method: method, expected: expected
           end
